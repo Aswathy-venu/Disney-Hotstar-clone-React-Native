46f62eca5f0281a28740264ed11ba639643c9848
Aswathy-venu <aswathyvenu201999@gmail.com>
2024-05-17 17:09:23 +0530

    Slider Creation

---
+++ DisneyHotstar/src/Components/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        body: "Thriller . Adventure . Drama . Friendship",
+        bgImgUrl: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/4736/1714809064736-i",
+        imgUrl:"https://img10.hotstar.com/image/upload/f_auto,h_124/sources/r1/cms/prod/8803/1714809038803-t",
+     },
+     {
+        body: "Thriller . Adventure . Drama . Friendship",
+        bgImgUrl: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/4736/1714809064736-i",
+        imgUrl:"https://img10.hotstar.com/image/upload/f_auto,h_124/sources/r1/cms/prod/8803/1714809038803-t",
+     },
+     {
+        body: "Thriller . Adventure . Drama . Friendship",
+        bgImgUrl: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/4736/1714809064736-i",
+        imgUrl:"https://img10.hotstar.com/image/upload/f_auto,h_124/sources/r1/cms/prod/8803/1714809038803-t",
+     },
+     {
+        body: "Thriller . Adventure . Drama . Friendship",
+        bgImgUrl: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/4736/1714809064736-i",
+        imgUrl:"https://img10.hotstar.com/image/upload/f_auto,h_124/sources/r1/cms/prod/8803/1714809038803-t",
+     },
+    {
+        body: "Thriller . Adventure . Drama . Friendship",
+        bgImgUrl: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/4736/1714809064736-i",
+        imgUrl:"https://img10.hotstar.com/image/upload/f_auto,h_124/sources/r1/cms/prod/8803/1714809038803-t",
+     },
+  ];
+  
+  export default data;
---
+++ DisneyHotstar/src/Screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { StyleSheet, SafeAreaView} from 'react-native';
+import CarouselCards from '../Components/CarouselCards'
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+    <CarouselCards />
+  </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
